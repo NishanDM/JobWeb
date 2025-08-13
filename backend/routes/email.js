2089cc475a8f27c1f60fdffc9495dde91a98fd69
Nishan Dhananjaya Morathota <nishandm97@gmail.com>
2025-08-13 06:57:51 +0530

    Delete backend/routes/email.js
    Deleted old unnecessary file

--- backend/routes/email.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendEmail } = require('../controllers/emailController');
-
-router.post('/send', sendEmail);  // <-- This should match /api/email/send
-
-module.exports = router;
