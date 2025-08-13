f7494ba8aae311f31a61989f06c1e9a33ecbe2c5
Nishan Dhananjaya Morathota <nishandm97@gmail.com>
2025-08-13 14:01:07 +0530

    Update completedJobRoutes.js

--- backend/routes/completedJobRoutes.js
+++ backend/routes/completedJobRoutes.js
@@ -1,9 +1,10 @@
 // routes/jobRoutes.js
 const express = require('express');
-const { getCompletedJobs } = require('../controllers/jobController');
+const { getCompletedJobs } = require('../config/jobController');
 
 const router = express.Router();
 
 router.get('/completed', getCompletedJobs);
 
 module.exports = router;
+
